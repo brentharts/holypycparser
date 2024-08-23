ddd75c36442c5f97e0f2ba019d3e51334f0f74b5
brent hartshorn <brenthartshorn@proton.me>
2024-08-22 19:08:33 -0700

    testing shared reg with `asm syntax

---
+++ examples/c_files/threads_shared_reg.c
@@ -0,0 +1,15 @@
+// thread id
+`0
+// registers
+`a0,a1,a2,a3,a4,a5,a6,a8
+`U0 ThreadA() {
+	while (1){
+		`gp += 1;
+	}
+}
+
+`1`s0,s1,s2,s3,s4,s5,s6,s8`U0 ThreadB() {
+	while (1){
+		`gp += 1;
+	}
+}
